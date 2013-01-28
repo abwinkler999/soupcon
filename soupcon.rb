32430326d79a1cfea242d00ed43d68010939633c
Benjamin Winkler <abwinkler999@gmail.com>
2013-01-28 00:28:36 -0500

    Fooling with nokogiri

--- soupcon.rb
+++ soupcon.rb
@@ -1,21 +1,31 @@
 require 'sinatra'
+require 'nokogiri'
+
 get '/' do
   erb :soupcon
 end
 
-def returnSomething
-  masterReturnString = ""
+def readRecipe
   Dir.foreach("assets") { |some_recipe|
     if some_recipe.include? ".ysr"
       recipeFile = File.new("assets/" + some_recipe);
       if recipeFile
       #IO.foreach("ravens.are") { |line| @area << line }
-        IO.foreach(recipeFile) { |line| masterReturnString << line }
+        #IO.foreach(recipeFile) { |line| masterReturnString << line }
+        @xmlDoc = Nokogiri::XML(recipeFile)
       else
         puts "File could not be opened!"
       end
     end
   }
-  return masterReturnString;
 end
-  
+
+def returnSomething
+  #returnString = Array.new
+  #@xmlDoc.xpath('//*').each do |elem|
+   # returnString << "Object Type: " + elem.class.to_s + " Name: " + elem +  " Key: " + elem.keys.to_s +  " Value: " + elem.values.to_s + "\n\r"
+  #end
+  #puts @xmlDoc.root
+  puts "Thing: " + @xmlDoc.xpath("//color")[0].to_s
+  return @xmlDoc.xpath("//color")
+end
