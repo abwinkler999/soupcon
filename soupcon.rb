839fed8ccc859258bf033daacb53934e47827afb
Benjamin Winkler <abwinkler999@gmail.com>
2013-01-28 22:40:32 -0500

    First working version

--- soupcon.rb
+++ soupcon.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
-require 'nokogiri'
+#require 'nokogiri'
+require 'plist'
 
 get '/' do
   erb :soupcon
@@ -7,12 +8,15 @@ end
 
 def readRecipe
   Dir.foreach("assets") { |some_recipe|
-    if some_recipe.include? ".ysr"
+    if some_recipe.include? ".bak"
       recipeFile = File.new("assets/" + some_recipe);
       if recipeFile
-      #IO.foreach("ravens.are") { |line| @area << line }
-        #IO.foreach(recipeFile) { |line| masterReturnString << line }
-        @xmlDoc = Nokogiri::XML(recipeFile)
+        @xmlDoc = Plist::parse_xml(recipeFile)
+        # for some reason, creates array with one element -- the hash
+        @resultingHash = @xmlDoc[0]
+        #puts @resultingHash.class
+        @resultingHash["RecipeName"] = some_recipe.to_s
+        #puts @resultingHash["RecipeName"]
       else
         puts "File could not be opened!"
       end
@@ -21,11 +25,23 @@ def readRecipe
 end
 
 def returnSomething
+  returnString = ""
   #returnString = Array.new
   #@xmlDoc.xpath('//*').each do |elem|
    # returnString << "Object Type: " + elem.class.to_s + " Name: " + elem +  " Key: " + elem.keys.to_s +  " Value: " + elem.values.to_s + "\n\r"
   #end
-  #puts @xmlDoc.root
-  puts "Thing: " + @xmlDoc.xpath("//color")[0].to_s
-  return @xmlDoc.xpath("//color")
+  #puts Plist::Emit.dump(@xmlDoc)
+  returnString << "<h1>" + @resultingHash["name"] + "</h1>"
+  @resultingHash.each {|key, value|
+    if key == "directions"
+      returnString << '<div id="directions">' + value + '</div>'
+    else
+      returnString << "<p>#{key} : #{value} </p>"
+    end
+  }
+  return returnString
+  #return "<p>Attribution: " + @resultingHash['attribution'] + "</p><p>Directions: " + @resultingHash['directions'] + "</p>" + @resultingHash['cuisine']
+  #return @resultingHash.inspect
+
+  #return "!"
 end
